eacaf91e450922325013c88f561542201bdae51b
GaurangGoel <gxg190015@utdallas.edu>
2023-09-17 20:00:45 -0500

    Latest

---
+++ js/textswitcher.js
@@ -0,0 +1,12 @@
+const textArr = ["नमस्ते दोस्त! 🙏🏻 ", "¡Hola amigo! 🙋🏻‍♂️ ", "Bonjour mon ami! 🖐🏻 ", "Hallo mein Freund! 👋🏻 ", "Ciao amico! 🙋🏻‍♂️ ", "Olá amigo! 🖐🏻 ", "你好朋友！👋🏻 ", "こんにちは、友達！🖐🏻 ", "안녕 친구! 🙋🏻‍♂️"]; 
+const textSwitch = document.getElementById("text-switch");
+
+setInterval(() => {
+  const randomIndex = Math.floor(Math.random() * textArr.length);
+  const randomNumber = Math.floor(Math.random() * 100);
+  if (randomNumber < 35) {
+    textSwitch.innerHTML = "Hi there! 👋🏻";
+  } else {
+    textSwitch.innerHTML = textArr[randomIndex];
+  }
+}, 775);
