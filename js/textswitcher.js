b87e342a9b6dea13d7cd088cfa064cc79b037cb1
GaurangGoel <89758277+GaurangGoel@users.noreply.github.com>
2023-09-18 16:40:23 -0500

    Update textswitcher.js

--- js/textswitcher.js
+++ js/textswitcher.js
@@ -1,6 +1,10 @@
-const textArr = ["नमस्ते दोस्त! 🙏🏻 ", "¡Hola amigo! 🙋🏻‍♂️ ", "Bonjour mon ami! 🖐🏻 ", "Hallo mein Freund! 👋🏻 ", "Ciao amico! 🙋🏻‍♂️ ", "Olá amigo! 🖐🏻 ", "你好朋友！👋🏻 ", "こんにちは、友達！🖐🏻 ", "안녕 친구! 🙋🏻‍♂️"]; 
+const textArr = ["नमस्ते दोस्त! 🙏🏻 ", "¡Hola amigo! 🙋🏻‍♂️ ", "Bonjour mon ami! 🖐🏻 ", "Hallo mein Freund! 👋🏻 ", "Ciao amico! 🙋🏻‍♂️ ", "Olá amigo! 🖐🏻 ", "你好朋友！👋🏻 ", "こんにちは、友達！🖐🏻 ", "안녕 친구! 🙋🏻‍♂️"];
 const textSwitch = document.getElementById("text-switch");
 
+// Set initial text to "Hi there!"
+textSwitch.innerHTML = "Hi there! 👋🏻";
+
+// Change the text at intervals
 setInterval(() => {
   const randomIndex = Math.floor(Math.random() * textArr.length);
   const randomNumber = Math.floor(Math.random() * 100);
